cdf34f598ad393d55b2a171a45b8d1e639ab318c
Samrith Shankar <samrith.shankar@dream11.com>
2025-01-13 12:52:47 +0530

    refactor: make data access easier within react code

--- web/src/data.tsx
+++ web/src/data.tsx
@@ -9,17 +9,17 @@ export type PerformanceDataEntry = {
 let data: PerformanceData = [];
 
 try {
-  data = await import('../../example/mock/log.json');
+  data = await import('../../example/mock/log.json').then((s) => s.default);
 } catch (error) {
   throw new Error(
     'Failed to load performance data at path ../../example/mock/log.json'
   );
 }
 
-export function useData(index?: number) {
-  if (typeof index === 'number' && index > -1) {
-    return data[index];
-  }
-
+export function useData(): PerformanceData {
   return data;
+}
+
+export function useDataEntry(index: number): PerformanceDataEntry | undefined {
+  return data[index];
 }
