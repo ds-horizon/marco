a98e1c008ded868c05966ce861a766c161ec1e75
shubhamguptadream11 <shubham.gupta@dream11.com>
2025-01-13 13:17:57 +0530

    feat: config changed

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,64 +1,66 @@
 // @ts-check
-import starlight from "@astrojs/starlight";
+import starlight from '@astrojs/starlight';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel';
 
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-const SITE = "https://github.com/dream-sports-labs/";
+const SITE = 'https://github.com/dream-sports-labs/';
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE,
+  site: 'https://github.com/dream-sports-labs.github.io',
+  base: 'marco',
+  outDir: 'static',
   integrations: [
     starlight({
-      title: "Marco",
+      title: 'Marco',
       // head: meta(SITE),
       pagination: true,
-      titleDelimiter: "/",
+      titleDelimiter: '/',
       description:
-        "Simple, composable, and effective state management for JavaScript.",
+        'Simple, composable, and effective state management for JavaScript.',
       logo: {
-        dark: "./src/assets/logo-dark.svg",
-        light: "./src/assets/logo-light.svg",
-        alt: "Marco Logo",
+        dark: './src/assets/logo-dark.svg',
+        light: './src/assets/logo-light.svg',
+        alt: 'Marco Logo',
         replacesTitle: true,
       },
       social: {
         github:
-          "https://github.com/dream-sports-labs/react-native-performance-tracker",
+          'https://github.com/dream-sports-labs/react-native-performance-tracker',
       },
       sidebar: [
         {
-          label: "Project",
-          autogenerate: { directory: "project" },
+          label: 'Project',
+          autogenerate: { directory: 'project' },
         },
         {
-          label: "API reference",
+          label: 'API reference',
           items: [
-            "api/static-configuration",
-            "api/tracking-screen",
-            "api/methods",
+            'api/static-configuration',
+            'api/tracking-screen',
+            'api/methods',
             {
-              label: "CLI",
+              label: 'CLI',
               items: [
-                "api/cli/overview",
-                "api/cli/generate-report",
-                "api/cli/visualization",
+                'api/cli/overview',
+                'api/cli/generate-report',
+                'api/cli/visualization',
               ],
             },
           ],
         },
         {
-          label: "Guide",
-          autogenerate: { directory: "guides" },
+          label: 'Guide',
+          autogenerate: { directory: 'guides' },
         },
       ],
-      customCss: ["./src/tailwind.css", "@fontsource-variable/inter"],
+      customCss: ['./src/tailwind.css', '@fontsource-variable/inter'],
       components: {
-        Head: "./src/overrides/head.astro",
+        Head: './src/overrides/head.astro',
       },
     }),
     tailwind({ applyBaseStyles: false }),
